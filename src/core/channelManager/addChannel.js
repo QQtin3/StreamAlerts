b4040797450d265e65952aa80b4eab7d4e7d9d44
QQtin <cdql@hotmail.fr>
2023-10-30 22:00:03 +0100

    Keep adding files + first preview build of the design

---
+++ src/core/channelManager/addChannel.js
@@ -0,0 +1,23 @@
+await function addChannel(name) {
+    let twitchChannelURL = `https://www.twitch.tv/${name}`;
+    if () {
+        return -1;
+    }
+    else {
+        if (isPartner()) {
+            return 2;
+        }
+        else {
+            return 1;
+        }
+    }
+}
+
+await function doesChannelExists(twitchChannelURL) {
+    fetch(
+
+    )
+}
+
+await function isPartner(twitchChannelURL) {
+}
